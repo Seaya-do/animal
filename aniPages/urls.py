8e54644ecef5988a0caceb640af075169f0c96a3
Seaya-do <67862904+Seaya-do@users.noreply.github.com>
2022-02-15 17:01:55 +0900

    django Study

--- aniPages/urls.py
+++ aniPages/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
-from aniPages import views
+from . import views
 
 urlpatterns = [
     path('index/', views.PostList.as_view()),
-    path('admin/', admin.site.urls),
+    path('<int:pk>/', views.single_post_page),
 
 ]
